322b24d19c225e6e3dbbf493321e202f2dc2a2e8
Hakan Bilgin <hbi99@hotmail.com>
2024-05-24 13:52:21 +0200

    updating test data

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,6 +1,7 @@
 
 let Test = {
 	init() {
+		setTimeout(() => window.find(`.teams-list .team:nth(1)`).trigger("click"), 500);
 		// setTimeout(() => window.find(`.toolbar-search_ input[name="field-search"]`).select(), 100);
 		// setTimeout(() => karaqu.shell("sys -o"), 300);
 	}
