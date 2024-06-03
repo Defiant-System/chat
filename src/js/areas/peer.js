92e14e3fa14e05dadcaa214a6310659566267166
Hakan Bilgin <hbi99@hotmail.com>
2024-06-03 20:25:45 +0200

    Update peer.js

--- src/js/areas/peer.js
+++ src/js/areas/peer.js
@@ -3,11 +3,10 @@
 
 {
 	connect() {
-		let Self = chat.peer;
+		let Self = chat.peer,
+			connection = Self.receiveConnection.bind(Self);
 		// establish connection; on events
-		Self.connection = window.peer.connect({
-			connection: Self.receiveConnection.bind(Self),
-		});
+		Self.connection = window.peer.connect({ connection });
 	},
 	sendFile(user, file) {
 		let Self = chat.peer,
@@ -18,17 +17,15 @@
 				size: file.size,
 				lastModified: file.lastModified,
 			};
-		console.log( file.lastModified );
+		// send file item
 		Self.fileConnection = Self.connection.connect(user.uuid);
 		Self.fileConnection.on("open", () => Self.fileConnection.send(item));
-		// Self.fileConnection.on("data", Self.receiveFile.bind(Self));
-		// Self.fileConnection.on("close", Self.disconnect.bind(Self));
+		Self.fileConnection.on("close", Self.disconnect.bind(Self));
 	},
 	receiveConnection(connection) {
 		let Self = chat.peer;
-
+		// receiver; connection
 		Self.fileConnection = connection;
-		// console.log( 1, connection );
 		Self.fileConnection.on("data", Self.receiveFile.bind(Self));
 	},
 	receiveFile(data) {
@@ -40,18 +37,20 @@
 			file = new File([data.buffer], data.name, info);
 
 		// download received file
-		window.download(file);
+		// window.download(file);
 		
 		// temp
 		Transmit.dispatch({ type: "done-file", el: chat.transcript.els.output.find(`.transmit-progress`) });
 
-		// Self.fileConnection.send({ b: "hellow 123" });
+		// close p2p connection
+		Self.disconnect();
 	},
 	disconnect() {
 		let Self = chat.peer;
 		// disconnect connection, if any
-		if (Self.connection) {
-			Self.connection.disconnect();
+		if (Self.fileConnection) {
+			Self.fileConnection.close();
 		}
+		// console.log("closed connection");
 	}
 }
